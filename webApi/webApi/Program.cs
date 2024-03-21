222a0cc7325473ca36633b36af3d71b50a0b4afc
Nicollas <132391248+NicollasDavi@users.noreply.github.com>
2024-03-21 11:34:27 -0300

    first route

--- webApi/webApi/Program.cs
+++ webApi/webApi/Program.cs
@@ -1,8 +1,38 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+using var client = new HttpClient();
+
+List<Produto> produtos = 
+[  
+    new Produto("Celular", "IOS"),
+    new Produto("Celular", "IOS"),
+    new Produto("Celular", "IOS"),
+    new Produto("Celular", "IOS")
+];
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/disgraca", () => "VAI DISGRAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+app.MapGet("/disgraca", () => produtos);
+
+app.MapPost("/cadastro", () => produtos);
+
+// app.MapGet("/viacep/{cep}", async (HttpContext context) => {
+//         try
+//         {
+//             var cep = context.Request.RouteValues["cep"];
+//             var response = await client.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+//             var content = await response.Content.ReadAsStringAsync();
+//             return content; 
+//         }
+//         catch (Exception)
+//         {
+//             throw new Exception("sabe deus oq deu rapa");
+//         }
+        
+    
+// });
 
 app.Run();
+
+
+record Produto(string nome, string desc);
